f5e1e84fedbf7fc53e596a39fc181a1cb72debbe
Yura Bema <75570059+LukeBrutale@users.noreply.github.com>
2024-06-13 14:25:33 +0300

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import s from './Header.module.css'
 
 function HeaderSection() {
   return (
-    <h1 className={s.header}> <span>Weather</span> Forecast</h1>
+    <h1 className={s.header}> <span>Weather</span>Cast</h1>
   )
 }
 
